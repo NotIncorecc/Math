006db054f055cc5b39279e744848d5a41c63572c
NotIncorecc <85992147+NotIncorecc@users.noreply.github.com>
2021-07-13 13:26:41 +0530

    Gives you pairs of Twin primes below a number
    and also an Interesting sequence

---
+++ twinprime.py
@@ -0,0 +1,71 @@
+number = int(input('enter a number and you get all the twin primes below it--'))
+n=[3,5, 11,13]
+
+def isprime(num):#will return true if number is prime
+    num=int(num)
+    st=2
+
+    while(num//2 >= st):
+        if(((num/st)-(int(num/st)))==0):#this is true when num/st is an integer which implies num cannot be a prime   
+            isp = False
+            break
+        st+=1
+        isp = True #if the above if condition doesn't run that would mean it was a prime
+    
+    return isp
+
+
+counter = 15
+twi_primes = '3 and 5\n11 and 13'#we'll check after 15
+
+if(number < 15):
+    print('Only twin primes below 15:-\n'+twi_primes)
+
+
+while (number >= counter):
+    if(isprime(counter)):
+        counter+=2
+        if(isprime(counter)):
+            second_num = counter
+            first_num = second_num-2
+            n.append(first_num)
+            n.append(second_num)
+            counter+=1
+            continue
+        else:
+            counter+=1
+            continue
+    else:
+        counter+=1
+        continue
+
+dar= [] 
+u = len(n)
+c1 = 1
+c2 = 2
+finl = ''
+
+
+def nthele(k, arr):
+    return arr[k-1]
+
+while c2 < u:
+    dn = n[c2] - n[c1]
+    dar.append(dn)
+    c1+=2
+    c2+=2
+
+dar.append(' ')
+
+m1, m2 = 1, 2
+d1 = 1
+while m2 <= u:
+    finl+= str(nthele(m1, n))+' and '+str(nthele(m2, n))+'\n'+str(nthele(d1, dar))+'\n'
+    m1+=2
+    m2+=2
+    d1+=1
+
+print(finl)
+print('The sequence-'," ".join(str(ko) for ko in dar))
+
+
